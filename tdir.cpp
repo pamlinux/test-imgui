3c8d66105b262c39d67a9fbdb00b240e8580a434
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-07-08 20:06:38 +0200

    Just to check new token with push.

--- tdir.cpp
+++ tdir.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <filesystem>
 namespace fs = std::filesystem;
+//namespace exp = std::experimental::filesystem;
 
 int main()
 {
-    std::string path = "C:\\Users\\moret";
+    //char sep = exp::filesystem::path::preferred_separator
+    std::string path = "/home/pam";
     for (const auto & entry : fs::directory_iterator(path))
         std::cout << entry.path() << std::endl;
 }
