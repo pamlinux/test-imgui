1bc9a48b898865f1e33696576d3caaa357ad8ae4
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-07-09 07:44:20 +0200

    Directory set to "../".

--- tdir.cpp
+++ tdir.cpp
@@ -6,7 +6,7 @@ namespace fs = std::filesystem;
 int main()
 {
     char sep = fs::path::preferred_separator;
-    std::string path = "/Users/pam";
+    std::string path = "../";
     for (const auto & entry : fs::directory_iterator(path))
         std::cout << entry.path() << std::endl; 
     std::cout << "Separator : " << sep << std::endl;
